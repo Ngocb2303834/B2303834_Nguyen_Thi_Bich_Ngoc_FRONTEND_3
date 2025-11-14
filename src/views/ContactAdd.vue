2883f3a528bb6b2fbc1883057641f4a61ac85c1a
Ngocb2303834 <ngocb2303834@student.ctu.edu.vn>
2025-11-14 22:58:58 +0700

    Tao trang them lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,9 +1,4 @@
 <template>
-    <!-- <div v-if="contact" class="page">
-        <h4>Thêm Liên hệ Mới</h4>
-        <ContactForm :contact="contact" @submit:contact="addContact" />
-        <p>{{ message }}</p>
-    </div> -->
     <div class="page">
         <h4>Thêm Liên hệ mới</h4>
         <ContactForm :contact="{}" @submit:contact="createContact" />
@@ -17,35 +12,14 @@ export default {
     components: {
         ContactForm,
     },
-    // props: {
-    //     id: { type: String, required: true },
-    // },
     data() {
         return {
-            // contact: null,
             message: "",
         };
     },
     methods: {
-        // async getContact(id) {
-        //     try {
-        //         this.contact = await ContactService.get(id);
-        //     } catch (error) {
-        //         console.log(error);
-        //         // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
-        //         this.$router.push({
-        //             name: "notfound",
-        //             params: {
-        //                 pathMatch: this.$route.path.split("/").slice(1)
-        //             },
-        //             query: this.$route.query,
-        //             hash: this.$route.hash,
-        //         });
-        //     }
-        // },
         async createContact(data) {
             try {
-                // await ContactService.create(this.contact._id, data);
                 await ContactService.create(data);
                 alert('Liên hệ được thêm thành công.');
                 this.$router.push({ name: "contactbook" });
@@ -55,7 +29,6 @@ export default {
         },
     },
     created() {
-        // this.getContact(this.id);
         this.message = "";
     },
 };
